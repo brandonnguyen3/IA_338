84df6276f2e390142faf54cfb9d470ae258d1b32
brandonnguyen3 <144299768+brandonnguyen3@users.noreply.github.com>
2024-03-04 17:53:40 -0700

    03/04 changed for req 2 FamilyRelation.java

--- GP3/edu/ucalgary/oop/FamilyRelation.java
+++ GP3/edu/ucalgary/oop/FamilyRelation.java
@@ -1,4 +1,12 @@
+/*
+ * 03/04
+ * Changes: made changes to adhere to requirement 2
+ * Have not tested. Should enforce a bi-directional relationship. 
+ */
+
 package edu.ucalgary.oop;
+
+import java.util.Objects;
 
 public class FamilyRelation {
     private DisasterVictim personOne;
@@ -7,21 +15,33 @@ public class FamilyRelation {
 
     // Constructor
     public FamilyRelation(DisasterVictim personOne, String relationshipTo, DisasterVictim personTwo) {
+        if (personOne == null || personTwo == null) {
+            throw new IllegalArgumentException("Both persons must be specified.");
+        }
         this.personOne = personOne;
         this.relationshipTo = relationshipTo;
         this.personTwo = personTwo;
+        personOne.addFamilyConnection(this); // Establish the two-sided relationship
+        personTwo.addFamilyConnection(this); // Establish the two-sided relationship
     }
 
-    // Getter and setter for personOne
+    // Getters and setters
+
     public DisasterVictim getPersonOne() {
         return personOne;
     }
 
     public void setPersonOne(DisasterVictim personOne) {
+        if (personOne == null) {
+            throw new IllegalArgumentException("Person cannot be null.");
+        }
+        // Remove the relationship from the previous personOne
+        this.personOne.removeFamilyConnection(this);
         this.personOne = personOne;
+        // Add the relationship to the new personOne
+        personOne.addFamilyConnection(this);
     }
 
-    // Getter and setter for relationshipTo
     public String getRelationshipTo() {
         return relationshipTo;
     }
@@ -30,12 +50,33 @@ public class FamilyRelation {
         this.relationshipTo = relationshipTo;
     }
 
-    // Getter and setter for personTwo
     public DisasterVictim getPersonTwo() {
         return personTwo;
     }
 
     public void setPersonTwo(DisasterVictim personTwo) {
+        if (personTwo == null) {
+            throw new IllegalArgumentException("Person cannot be null.");
+        }
+        // Remove the relationship from the previous personTwo
+        this.personTwo.removeFamilyConnection(this);
         this.personTwo = personTwo;
+        // Add the relationship to the new personTwo
+        personTwo.addFamilyConnection(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FamilyRelation that = (FamilyRelation) o;
+        return Objects.equals(personOne, that.personOne) &&
+                Objects.equals(relationshipTo, that.relationshipTo) &&
+                Objects.equals(personTwo, that.personTwo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(personOne, relationshipTo, personTwo);
     }
 }
